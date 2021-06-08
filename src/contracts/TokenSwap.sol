3138f394da0b1b3828493ee604a7640efd2ccb4b
tunde <icepaigez@gmail.com>
2021-06-08 11:44:52 +0100

    created a migration file for the contract

--- src/contracts/TokenSwap.sol
+++ src/contracts/TokenSwap.sol
@@ -3,19 +3,14 @@ pragma solidity ^0.5.0;
 contract TokenSwap {
 	string public name = "TokenSwap DEX";
 
-	//2. exchange token for fiat i.e when you sell your tokens
-	function transferTokenToBuyer(uint _tokenAmount) public {
-		//check the type of token being swapped[erc20, ether] 
-		//and ensure that they have the amount + fees
+	event Sent(address from, address to, uint amount);
 
+	//2. transfer ether or token to another account from one account
+	function transferEthToBuyer(uint _ethAmount, address _buyer) public {
+		require(balances[msg.sender] >= _ethAmount);
+		balances[msg.sender] -= _ethAmount;
+		balances[_buyer] += _ethAmount;
+		emit Sent(msg.sender, _buyer, _ethAmount);
 	}
 
-	function transferEthToBuyer(uint _ethAmount) public {
-		
-	}
-
-	//5. send token out of the DEX
-	function sendTokens() public {
-
-	}
 }
