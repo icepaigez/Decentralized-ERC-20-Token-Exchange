50ed5b06d05eb1e401c0393bf8def97c40b577da
tunde <icepaigez@gmail.com>
2021-06-08 12:05:56 +0100

    updating the smart contract

--- src/contracts/TokenSwap.sol
+++ src/contracts/TokenSwap.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.5.0;
 contract TokenSwap {
 	string public name = "TokenSwap DEX";
 
+	address owner;
+
 	event Sent(address from, address to, uint amount);
 
-	//2. transfer ether or token to another account from one account
-	function transferEthToBuyer(uint _ethAmount, address _buyer) public {
-		require(balances[msg.sender] >= _ethAmount);
-		balances[msg.sender] -= _ethAmount;
-		balances[_buyer] += _ethAmount;
-		emit Sent(msg.sender, _buyer, _ethAmount);
+	constructor() {
+		owner = msg.sender;
 	}
 
+	//2. transfer ether or token to another account from one account
+	function transferEthToBuyer(uint _ethAmount, address _buyer) public payable {
+		require(owner.balance >= _ethAmount);
+		_buyer.transfer(_ethAmount);
+		emit Sent(msg.sender, _buyer, _ethAmount);
+	}
 }
