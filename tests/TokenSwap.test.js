c5987c045774bc207a0f0d8ce4258c0c930c7e5e
tunde <icepaigez@gmail.com>
2021-06-09 08:20:50 +0100

    adding erc20 token transfer options to the dex

--- tests/TokenSwap.test.js
+++ tests/TokenSwap.test.js
@@ -1,5 +1,10 @@
 const TokenSwap = artifacts.require("TokenSwap");
 
+
+function tokens(qty) {
+	return web3.utils.toWei(qty, "ether");
+}
+
 contract("TokenSwap", accounts => {
 	let contract;
 	before(async() => {
