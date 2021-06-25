d59d561b095fd5a82093da2953ffe8255d00dbc0
tunde <icepaigez@gmail.com>
2021-06-25 20:56:07 +0100

    created an additional token to test token pair pool liquidity

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,5 @@
 const DAppToken = artifacts.require("DAppToken");
+const TeaToken = artifacts.require("TeaToken");
 const LPToken = artifacts.require("LPToken");
 const TokenSwap = artifacts.require("TokenSwap");
 
@@ -6,12 +7,14 @@ const TokenSwap = artifacts.require("TokenSwap");
 
 module.exports = async function(deployer) {
   await deployer.deploy(DAppToken);
+  await deployer.deploy(TeaToken);
   await deployer.deploy(LPToken);
 
   const dapp = await DAppToken.deployed();
   const lpt = await LPToken.deployed();
+  const tea = await TeaToken.deployed();
 
-  await deployer.deploy(TokenSwap, dapp.address, lpt.address);
+  await deployer.deploy(TokenSwap, dapp.address, lpt.address, tea.address);
   const dex = await TokenSwap.deployed();
   
   await lpt.transfer(dex.address, "1000000000000000000000000");
