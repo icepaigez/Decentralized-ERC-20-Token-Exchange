a28e818bca2935bc8762ce2df3fab1e35b97d770
tunde <icepaigez@gmail.com>
2021-06-25 19:36:38 +0100

    updating the dex smart contract tests

--- test/TokenSwap.test.js
+++ test/TokenSwap.test.js
@@ -38,10 +38,12 @@ contract("TokenSwap", accounts => {
 	})
 
 	describe("Transfer ETH/TOKEN pair to the contract", async() => {
-		let trf;
+		let trf, poolName;
 		before(async() => {
+			let tok1Symbol = await token1.symbol();
+			poolName = `${tok1Symbol}-${'ETH'}`
 			await token1.approve(dex.address, tokens("1"), {from:accounts[0]});
-			trf = await dex.addEthLiquid(tokens("1"), {from:accounts[0], value:tokens("1.5")});
+			trf = await dex.initEthPair(tokens("1"), poolName, {from:accounts[0], value:tokens("1.5")});
 		})
 
 		it('should accept a pair that contains ETH and an ERC-20 Token', async() => {
@@ -50,12 +52,22 @@ contract("TokenSwap", accounts => {
 			let dexTokenBalance = await token1.balanceOf(dex.address);
 			let lpBalance = await dex.liquidity(accounts[0]);
 			let dexBalance = await dex.dexLiquidity();
+			let pair = await dex.returnPairs();
 
 			assert.equal(lpBalance, tokens("2.5"));
 			assert.equal(dexBalance.toString(), tokens("2.5"));
 			assert.equal(lpTokenBalance, tokens("999999"));
 			assert.equal(dexEthBalance, tokens("1.5"));
 			assert.equal(dexTokenBalance, tokens("1"));
+			assert.equal(pair[0], poolName);
+		})
+	})
+
+	describe('Update liquidity provider token portion', async() => {
+		it('should update the ownership proportion of total liquidity provider token for additional liquidity', async() => {
+			await dex.issueLPToken(accounts[0], tokens("100"));
+			let tokenPortion = await dex.lptokenOwn(accounts[0]);
+			assert.equal(tokenPortion, tokens("100"));
 		})
 	})
 })  
