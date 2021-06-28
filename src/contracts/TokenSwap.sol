6d20037c63a84db8c59eaf51bfdca216c255f611
tunde <icepaigez@gmail.com>
2021-06-28 20:10:40 +0100

    updated the trade functions in the dex smart contract

--- src/contracts/TokenSwap.sol
+++ src/contracts/TokenSwap.sol
@@ -24,7 +24,8 @@ contract TokenSwap {
 
 	string[] public pairs;
 	mapping (address => uint256) public liquidity;
-	mapping (address => mapping (string => uint256)) public poolLiquidity;
+	mapping (address => mapping (string => uint256)) public totalLiquidity;
+	mapping (address => mapping (string => mapping (string => uint256))) public poolLiquidity;
 	mapping (address => mapping (string => uint256)) public lptokenOwned;
 	mapping (string => uint256) public pool; //ETH-DApp: "500"
 	mapping (string => mapping (string => uint256)) public poolPair; //ETH-DApp[ETH]:100, ETH-DApp[DApp]:400 
@@ -49,7 +50,9 @@ contract TokenSwap {
 		liquidity[msg.sender] += _tokenAmount + msg.value;
 		dexLiquidity += _tokenAmount + msg.value;
 		pool[_pairName] += _tokenAmount + msg.value;
-		poolLiquidity[msg.sender][_pairName] += _tokenAmount + msg.value;
+		totalLiquidity[msg.sender][_pairName] += _tokenAmount + msg.value;
+		poolLiquidity[msg.sender][_pairName][_pair1] += msg.value;
+		poolLiquidity[msg.sender][_pairName][_pair2] += _tokenAmount;
 		poolPair[_pairName][_pair1] += msg.value;
 		poolPair[_pairName][_pair2] += _tokenAmount;
 		pairs.push(_pairName);
@@ -61,7 +64,9 @@ contract TokenSwap {
 		liquidity[msg.sender] += _tokenAmount + msg.value;
 		dexLiquidity += _tokenAmount + msg.value;
 		pool[_pairName] += _tokenAmount + msg.value;
-		poolLiquidity[msg.sender][_pairName] += _tokenAmount + msg.value;
+		totalLiquidity[msg.sender][_pairName] += _tokenAmount + msg.value;
+		poolLiquidity[msg.sender][_pairName][_pair1] += msg.value;
+		poolLiquidity[msg.sender][_pairName][_pair2] += _tokenAmount;
 		poolPair[_pairName][_pair1] += msg.value;
 		poolPair[_pairName][_pair2] += _tokenAmount;
 	}
@@ -73,7 +78,9 @@ contract TokenSwap {
 		liquidity[msg.sender] += _token1Amount + _token2Amount;
 		dexLiquidity += _token1Amount + _token2Amount;
 		pool[_pairName] += _token1Amount + _token2Amount;
-		poolLiquidity[msg.sender][_pairName] += _token1Amount + _token2Amount;
+		totalLiquidity[msg.sender][_pairName] += _token1Amount + _token2Amount;
+		poolLiquidity[msg.sender][_pairName][_pair1] += _token1Amount;
+		poolLiquidity[msg.sender][_pairName][_pair2] += _token2Amount;
 		poolPair[_pairName][_pair1] += _token1Amount;
 		poolPair[_pairName][_pair2] += _token2Amount;
 		pairs.push(_pairName);
@@ -86,15 +93,17 @@ contract TokenSwap {
 		liquidity[msg.sender] += _token1Amount + _token2Amount;
 		dexLiquidity += _token1Amount + _token2Amount;
 		pool[_pairName] += _token1Amount + _token2Amount;
-		poolLiquidity[msg.sender][_pairName] += _token1Amount + _token2Amount;
+		totalLiquidity[msg.sender][_pairName] += _token1Amount + _token2Amount;
+		poolLiquidity[msg.sender][_pairName][_pair1] += _token1Amount;
+		poolLiquidity[msg.sender][_pairName][_pair2] += _token2Amount;
 		poolPair[_pairName][_pair1] += _token1Amount;
 		poolPair[_pairName][_pair2] += _token2Amount;
 	}
 
 	function tradeEthforToken(string memory _pairName, string memory _pair1, string memory _pair2) public payable returns(uint256) {
-		require(dexLiquidity > 0);
 		uint256 pair1Balance = poolPair[_pairName][_pair1];
 		uint256 pair2Balance = poolPair[_pairName][_pair2];
+		require(pair1Balance > 0 && pair2Balance > 0);
 		uint256 poolConstant = pair1Balance * pair2Balance;
 		uint256 inputAmount = msg.value;
 		uint256 inputAmountWithFee = inputAmount.mul(997);
@@ -103,15 +112,36 @@ contract TokenSwap {
 		uint256 denominator = inputAmountWithFee.add(pair1BalanceWithFee);
 		uint256 postTradePair2Balance = numerator / denominator;
 		uint256 tokenTradeValue = pair2Balance.sub(postTradePair2Balance);
+		require(pair2Balance > tokenTradeValue);
 		token.transfer(msg.sender, tokenTradeValue);
 		poolPair[_pairName][_pair1] += inputAmount;
 		poolPair[_pairName][_pair2] -= tokenTradeValue;
 		uint256 poolBal = pool[_pairName];
-		// pool[_pairName] += inputAmount - tokenTradeValue;
 		poolBal = poolBal.add(inputAmount).sub(tokenTradeValue);
-		// dexLiquidity += inputAmount - tokenTradeValue;
 		dexLiquidity = dexLiquidity.add(inputAmount).sub(tokenTradeValue);
 		return tokenTradeValue;
+	}
+
+	function tradeTokenforEth(uint256 _tokenAmount, string memory _pairName, string memory _pair1, string memory _pair2) public returns(uint256) {
+		uint256 pair1Balance = poolPair[_pairName][_pair1];
+		uint256 pair2Balance = poolPair[_pairName][_pair2];
+		require(pair1Balance > 0 && pair2Balance > 0);
+		uint256 poolConstant = pair1Balance * pair2Balance;
+		uint256 inputAmount = _tokenAmount;
+		uint256 inputAmountWithFee = inputAmount.mul(997);
+		uint256 numerator = poolConstant.mul(1000);
+		uint256 denominator = pair2Balance.mul(1000).add(inputAmountWithFee);
+		uint256 postTradePair1Balance = numerator / denominator;
+		uint256 etherTradeValue = pair1Balance.sub(postTradePair1Balance);
+		require(pair1Balance > etherTradeValue);
+		address payable trader = payable(msg.sender);
+		trader.transfer(etherTradeValue);
+		poolPair[_pairName][_pair1] -= etherTradeValue;
+		poolPair[_pairName][_pair2] += inputAmount;
+		uint256 poolBal = pool[_pairName];
+		poolBal = poolBal.add(inputAmount).sub(etherTradeValue);
+		dexLiquidity = dexLiquidity.add(inputAmount).sub(etherTradeValue);
+		return etherTradeValue;
 	}
 }
 
