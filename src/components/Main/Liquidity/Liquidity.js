1513144a24974ea7df0833e7e37b722a0f1ba8a0
tunde <icepaigez@gmail.com>
2021-07-05 15:27:48 +0100

    setting up the liquidity provision form

--- src/components/Main/Liquidity/Liquidity.js
+++ src/components/Main/Liquidity/Liquidity.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "./liquidity.css";
+import ethLogo from "../eth-logo.png";
+import tokenLogo from "../token-logo.png";
 
 class Liquidity extends Component {
 	render() {
@@ -9,24 +11,24 @@ class Liquidity extends Component {
 					<div className="pool__data">
 					  <p>Total Tokens Locked</p>
 					  <div className="pool__pair">
-					    <img src=""/>
+					    <img src={tokenLogo} alt="" height='30'/>
 					    <p>USDC</p>
 					    <p className="token__value">100000</p>
 					  </div>
 					  <div className="pool__pair">
-					    <img src=""/>
+					    <img src={ethLogo} alt="" height='30'/>
 					    <p>ETH</p>
 					    <p className="token__value">1000</p>
 					  </div>
 					</div>
 				</div>
 				<div className="pool__liquid">
-					<form>
+					<form className="liquidity__form">
 					  <div>
 					    <input type="text" placeholder="0" name="pool" disabled/>
-					    <div>
+					    <div className="pairs">
 					      <select name="pools" id="pools">
-					        <option value="eth__dapp" selected>ETH-DApp</option>
+					        <option value="eth__dapp" defaultValue>ETH-DApp</option>
 					        <option value="eth__tea">ETH-TEA</option>
 					        <option value="dapp__tea">DApp-TEA</option>
 					      </select>
@@ -34,16 +36,16 @@ class Liquidity extends Component {
 					  </div>
 					  <div>
 					    <input type="text" name="" placeholder="0"/>
-					    <div>
-					       <img src="" height='32' alt=""/>
-					       &nbsp;&nbsp;&nbsp; ETH
+					    <div className="pairs">
+					       <img src={ethLogo} height='32' alt=""/>
+					        ETH
 					    </div>
 					  </div>
 					  <div>
 					    <input type="text" name="" placeholder="0"/>
-					    <div>
-					       <img src="" height='32' alt=""/>
-					       &nbsp;&nbsp;&nbsp; DApp
+					    <div className="pairs">
+					       <img src={tokenLogo} height='32' alt=""/>
+					       DApp
 					    </div>
 					  </div>
 					  <button type="submit">Provide Liquidity</button>
