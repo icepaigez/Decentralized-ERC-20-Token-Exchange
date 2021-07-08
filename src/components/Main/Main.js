c58806711b95e7029740e041fe9981143777a630
tunde <icepaigez@gmail.com>
2021-07-08 09:08:30 +0100

    added buttons for liquidity and trade

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,6 +7,10 @@ class Main extends Component {
 		const { pools, ethLiquid, dex, web3, tokenLiquid } = this.props;
 		return(
 			<div className="main">
+				<div className="option">
+					<button className="liquid">Liquidity</button>
+					<button className="trade">Trade</button>
+				</div>
 				<Liquidity tokenLiquid={tokenLiquid} web3={web3} dex={dex} pools={pools} ethLiquid={ethLiquid}/>
 			</div>
 		)
---
+++ src/components/Main/Trade/Trade.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./trade.css";
+
+class Trade extends Component {
+	render() {
+		return(
+			<div>Trader</div>
+		)
+	}
+}
+
+export default Trade;
